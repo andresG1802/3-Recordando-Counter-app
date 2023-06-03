f57d359556b1c38434196c4b305e998ac27a768f
andresG1802 <andresdaniel.go2020@gmail.com>
2023-06-02 21:09:14 -0500

    Fin de seccion 3 Counter app

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,20 +1,43 @@
-
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 export const CounterApp = ({value}) => {
-    
+
+    const [ counter , setCounter] = useState( 0 );
+
+    const handleAdd = () => {
+        // Si pongo counter++ daria error por que es un const
+        setCounter(counter + 1);
+        // setCounter((c)=> c + 1)
+    }
+    const handleSubtract = ( )=>{
+        setCounter(counter -1 );
+    }
+    const handleReset = () =>{
+        setCounter(0);
+    }
     return (
         <>
             <h1>Counter App</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
+            
+            <button onClick={ handleAdd }>
+                +1
+            </button>
+            <button onClick={ handleSubtract }>
+                -1
+            </button>
+            <button onClick={ handleReset }>
+                
+            </button>
         </>
     );
 }
 
 CounterApp.propTypes = {
-    value:PropTypes.number,
+    value:PropTypes.number
 }
 
-CounterApp.defaultProps = {
-    value:2
-}
+// CounterApp.defaultProps = {
+//     value:10
+// }
